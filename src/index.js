7fcb989fe3230eb0b90388d86a1998ebac4632f3
IldarAbdulin <abdulinildar1@gmail.com>
2022-12-30 20:26:54 +0600

    fixed bug

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.scss'
 import { Provider } from 'react-redux';
 import { App } from './App';
-import {store, persistor} from './store'
+import {cofigureStore, persistor} from './store'
 import { PersistGate } from 'redux-persist/integration/react';
+
+const store = cofigureStore()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </React.StrictMode>
 );
+{/* <PersistGate loading={null} persistor={persistor}>
+</PersistGate> */}
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,32 @@
 import { createStore } from "redux";
-import {persistStore, persistReducer} from 'redux-persist'
-import storage from "redux-persist/lib/storage";
+// import {persistStore, persistReducer} from 'redux-persist'
+// import storage from "redux-persist/lib/storage";
+import {loadState, saveState} from './localStorage'
 
 import { reducer } from './root-reducer';
 
-const persistConfig = {
-  key: 'root',
-  storage
+// const persistConfig = {
+//   key: 'root',
+//   storage
+// }
+// const persistedReducer = persistReducer(persistConfig, reducer)
+// export const store = createStore(
+//   persistedReducer,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )
+// export const persistor = persistStore(store)
+
+export const cofigureStore = () => {
+  const persistedState = loadState()
+  const store = createStore(
+    reducer,
+    persistedState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+  store.subscribe(() => {
+    saveState({
+      todos: store.getState().todos
+    })
+  })
+  return store
 }
-const persistedReducer = persistReducer(persistConfig, reducer)
-export const store = createStore(
-  persistedReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
-export const persistor = persistStore(store)
